0597239facea125b4fe1dba7e4a7a1a12ae0712f
zriback <zacharythecdr@gmail.com>
2025-11-19 09:34:56 -0800

    Update compile script to be more similar to others

--- bech/demo/bad_function/compile.sh
+++ bech/demo/bad_function/compile.sh
@@ -1,15 +1,47 @@
 #!/bin/bash
-set -euo pipefail
 
-SRC="bad.c"
-BC="${SRC%.c}.bc"
-LL="${SRC%.c}.ll"
+#因为不是makefile，先删除可能干扰的ir文件
+rm -rf *.bc
+rm -rf *.ll
 
-rm -f "$BC" "$LL"
 
-clang-12 -c -emit-llvm -O0 -g -gcolumn-info -Xclang -disable-O0-optnone \
-  -DMEMORY_MODULE_NO_REUSE_IMPLS -fcolor-diagnostics \
-  -I/home/user/CT_Prover/smack/share/smack/include "$SRC" -o "$BC"
+build(){
+# deps, source, headfile_path, otherflg
 
-llvm-dis-12 "$BC" -o "$LL"
-rm -f "$BC"
+finalbc=${source//.c/.bc}
+finalll=${source//.c/.ll}
+#生成dep文件
+for i in ${deps[@]}; do 
+clang-12 -c -emit-llvm -O0 -g -gcolumn-info -Xclang -disable-O0-optnone -DMEMORY_MODULE_NO_REUSE_IMPLS -fcolor-diagnostics $headfile_path $otherflg $i
+done
+
+#生成wrapper文件
+clang-12 -c -emit-llvm -O0 -g -gcolumn-info -Xclang -disable-O0-optnone -DMEMORY_MODULE_NO_REUSE_IMPLS -fcolor-diagnostics $headfile_path -I/home/user/CT_Prover/smack/share/smack/include $source
+
+#链接
+if [ ${#deps[@]} -ne 0 ]; then
+llvm-link-12 *.bc -o $finalbc
+fi
+
+llvm-dis-12 $finalbc
+opt-12 -mem2reg -S $finalll -o $finalll
+
+rm *.bc
+
+}
+
+
+
+# 四个需要指定的选项
+deps=( )
+
+source=bad.c
+
+headfile_path="-I../BearSSL/src \
+               -I../BearSSL/inc"
+
+otherflg=""
+
+build deps, source, headfile_path, otherflg
+
+
